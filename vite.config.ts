0e54fe5f44d7525a1ec4d11f17c28d5cdea2805b
ouattaraissouf7507 <ouattaraissouf7507@gmail.com>
2026-05-18 14:07:58 +0000

    fix bug clientlo

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:4000",
+        // target: "http://localhost:4000llllll",
         target: "https://api.marabucoo.marabu.services",
         changeOrigin: true,
       },
